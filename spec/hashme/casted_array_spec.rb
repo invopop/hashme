385d271e46a57387e55bb8d896163980577c7994
Luismi Cavallé <luismi@lmcavalle.com>
2023-01-12 14:32:14 +0000

    Refactor spec to ease adding new examples

--- spec/hashme/casted_array_spec.rb
+++ spec/hashme/casted_array_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 describe Hashme::CastedArray do
-
   let :owner do
     double()
   end
@@ -13,43 +12,32 @@ describe Hashme::CastedArray do
     end
   end
 
-  describe "#initialize" do
-    let :property do
-      Hashme::Property.new(:name, String)
-    end
-    before :each do
-      @obj = Hashme::CastedArray.new(property, owner, ['test'])
-    end
+  let :property do
+    Hashme::Property.new(:item, submodel)
+  end
 
+  subject do
+    Hashme::CastedArray.new(property, owner, [{:name => 'test'}])
+  end
+
+  describe "#initialize" do
     it "should prepare array" do
-      expect(@obj.length).to eql(1)
+      expect(subject.length).to eql(1)
     end
 
     it "should set property" do
-      expect(@obj.property).to eql(property)
+      expect(subject.property).to eql(property)
     end
 
     it "should instantiate and cast each value" do
-      expect(@obj.first).to eql("test")
-      expect(@obj.first.class).to eql(String)
-    end
-  end
-
-  describe "adding to array" do
-    subject do
-      Hashme::CastedArray.new(property, owner, [{:name => 'test'}])
-    end
-    let :property do
-      Hashme::Property.new(:item, submodel)
-    end
-
-    it "should cast new items" do
-      subject << {:name => 'test2'}
-      expect(subject.last.class).to eql(submodel)
+      expect(subject.first.class).to eql(submodel)
       expect(subject.first.name).to eql('test')
-      expect(subject.last.name).to eql('test2')
     end
-
   end
 
+  it "should cast items added to the array" do
+    subject << {:name => 'test2'}
+    expect(subject.last.class).to eql(submodel)
+    expect(subject.last.name).to eql('test2')
+  end
 end
